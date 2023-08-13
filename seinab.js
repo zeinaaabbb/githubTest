0df8b4b53a0ce8e82f74b67b5a8b1e35b5f4f54e
Seinab Warsama <seinabwarsama@Seinabs-Air.cust.communityfibre.co.uk>
2023-08-13 15:52:24 +0100

    more stuff

--- seinab.js
+++ seinab.js
@@ -1,10 +1,19 @@
-hello123 testing
 
-hello hello
+function add(a, b) {
+ const seinab = 6 + b;
+ 
+ console.log('function entered'); 
+ console.log(seinab);
 
-hello 111
+ console.log(`my name is ${seinab}`)
+send(seinab)
+console.log(`ahmed is ${ahmed}`)
+}
 
-dsdsadsad
-asdas
-debuggerasd
-asd
+function send(name) {
+    console.log(name)
+const ahmed = name + 4
+console.log(`ahmed is ${ahmed} years old`);
+return ahmed
+}
+add(7, 9);
