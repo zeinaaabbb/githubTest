727dd8dc33d81eb983e7f4bd2712c424152d2b8d
zeinab <seinabwarsama@Zeinabs-Air.cust.communityfibre.co.uk>
2023-08-26 21:37:43 +0100

    learnings

--- nodelearnings/codecademy.js
+++ nodelearnings/codecademy.js
@@ -2,4 +2,4 @@ const querystring = require("querystring");
 
 const url = "https://www.example.com/p/a/t/h?course=node&lesson=http";
 
-const queryToParse = querystring.split();
+const queryToParse = querystring.split('?');
