e6655515cb78dbd2eaa1751e441fa29e88c1bf52
zeinaaabbb <129793281+zeinaaabbb@users.noreply.github.com>
2023-08-27 14:56:34 +0100

    codecademy.js

--- nodelearnings/codecademy.js
+++ nodelearnings/codecademy.js
@@ -3,3 +3,5 @@ const querystring = require("querystring");
 const url = 'https://www.example.com/p/a/t/h?course=node&lesson=http';
 
 const queryToParse = querystring.split('?')[1];
+
+console.log(queryToParse); 
